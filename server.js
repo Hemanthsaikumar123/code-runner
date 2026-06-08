088509def54ab14479136bbd9d7068e8e6b6de0d
koppuhemanthsaikumar <koppuhemanthsaikumar123@gmail.com>
2026-06-08 15:37:53 +0530

    Setup Docker Compose with Node and PostgreSQL

---
+++ server.js
@@ -0,0 +1,76 @@
+require("dotenv").config();
+const express = require("express");
+const { Pool } = require("pg");
+
+
+const app = express();
+app.use(express.json());
+
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+});
+
+app.get("/", async (req, res) => {
+  res.send("Welcome to Code Runner API");
+});
+
+app.post("/submission", async (req, res) => {
+  try {
+    const { language, code } = req.body;
+
+    const result = await pool.query(
+      `
+      INSERT INTO submissions(language, code)
+      VALUES($1, $2)
+      RETURNING *
+      `,
+      [language, code]
+    );
+
+    res.status(201).json(result.rows[0]);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: err.message
+    });
+  }
+});
+
+app.get("/submission/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await pool.query(
+      `
+      SELECT *
+      FROM submissions
+      WHERE id = $1
+      `,
+      [id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "Submission not found"
+      });
+    }
+
+    res.json(result.rows[0]);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: err.message
+    });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
